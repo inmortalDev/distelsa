3c8d3e36fee6c74be702156f2041a06c2a9d8c22
andres <xandresxandresx@hotmail.com>
2021-03-26 02:29:14 +0000

    Segundo commit

--- app/Models/Departamento.php
+++ app/Models/Departamento.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Departamento extends Model
 {
-    use HasFactory;
+    protected $table='departamentos';
 }
